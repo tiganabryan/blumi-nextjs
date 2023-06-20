4e1e40324a4e7cdd875b602f93d078b238e4c4c2
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2023-06-20 09:54:26 -0400

    change home page typography to h1 and h2 for seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import IndexView from 'views/IndexView';
+// import IndexView from 'views/IndexView';
+import Head from 'next/head';
 import DesignCompanyPage from './design-company';
 
 const IndexPage = () => {
-  return <DesignCompanyPage />;
+  return (
+    <>
+      <Head>
+        <title>blumi design lorem lorem etc</title>
+        <meta
+          name="description"
+          content="lorem ipsum dolor sit amet consectetur adipisicing elit."
+          key="desc"
+        />
+      </Head>
+      <DesignCompanyPage />
+    </>
+  );
 };
 
 export default IndexPage;
