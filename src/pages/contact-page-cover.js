25d3caf5810fee60b09a77281c52ecfc42811a35
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2023-06-21 15:01:44 -0400

    add title, description, amd keywords to contact page

--- src/pages/contact-page-cover.js
+++ src/pages/contact-page-cover.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import Head from 'next/head';
 import ContactPageCover from 'views/ContactPageCover';
 
 const ContactCoverPage = () => {
+  const seo = {
+    title: 'contact us | blumi',
+    description: "blumi design's websites.",
+    keywords: [
+      'affordable web design toronto',
+      'web design for small businesses',
+      'wordpress website maintenance toronto',
+    ],
+  };
+
+  const { title, description, keywords } = seo;
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} key="desc" />
+        <meta name="keywords" content={keywords.join(', ')} />
+      </Head>
+
+      <ContactPageCover />
+    </>
+  );
   return <ContactPageCover />;
 };
 
