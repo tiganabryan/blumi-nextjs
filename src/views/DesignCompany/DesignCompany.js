9a6207d49f8efe86970a5c73e925d02350b507cc
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2023-07-03 21:36:44 -0400

    remove main component from designcompany.js
    get rid of nav and footer

--- src/views/DesignCompany/DesignCompany.js
+++ src/views/DesignCompany/DesignCompany.js
@@ -28,50 +28,50 @@ const DesignCompany = () => {
     <>
       {/* <HeroVector viewBoxWidth={viewBoxWidth} viewBoxHeight={viewBoxHeight} /> */}
 
-      <Main>
-        {/* <Container> */}
-        <Hero />
-        {/* </Container> */}
+      {/* <Main> */}
+      {/* <Container> */}
+      <Hero />
+      {/* </Container> */}
+      <Container>
+        <Partners />
+      </Container>
+      <Container>
+        <CaseStudy1 />
+      </Container>
+      <Container paddingY={'0 !important'}>
+        <CaseStudy2 />
+      </Container>
+      <Container>
+        <CaseStudy3 />
+      </Container>
+      <Box
+        position={'relative'}
+        sx={{
+          backgroundColor: theme.palette.alternate.main,
+        }}
+      >
         <Container>
-          <Partners />
-        </Container>
-        <Container>
-          <CaseStudy1 />
-        </Container>
-        <Container paddingY={'0 !important'}>
-          <CaseStudy2 />
-        </Container>
-        <Container>
-          <CaseStudy3 />
+          <Contact />
         </Container>
         <Box
-          position={'relative'}
+          component={'svg'}
+          preserveAspectRatio="none"
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          viewBox="0 0 1920 100.1"
           sx={{
-            backgroundColor: theme.palette.alternate.main,
+            width: '100%',
+            marginBottom: theme.spacing(-1),
           }}
         >
-          <Container>
-            <Contact />
-          </Container>
-          <Box
-            component={'svg'}
-            preserveAspectRatio="none"
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            viewBox="0 0 1920 100.1"
-            sx={{
-              width: '100%',
-              marginBottom: theme.spacing(-1),
-            }}
-          >
-            <path
-              fill={theme.palette.background.paper}
-              d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
-            ></path>
-          </Box>
+          <path
+            fill={theme.palette.background.paper}
+            d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
+          ></path>
         </Box>
-      </Main>
+      </Box>
+      {/* </Main> */}
     </>
   );
 };
