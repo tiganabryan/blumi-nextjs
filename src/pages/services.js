7049f090b80df0163876a98aafdb37f46ae6dea8
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2023-11-17 15:39:11 -0500

    add services page, make card click send user to an id on Services page

---
+++ src/pages/services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div style={{ margin: '80rem' }} id="idea">
+      Idea
+    </div>
+  );
+};
+
+export default Services;
--- src/views/DesignCompany/components/Services/Services.jsx
+++ src/views/DesignCompany/components/Services/Services.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
-import { Typography } from '@mui/material';
+import { Typography, CardActionArea } from '@mui/material';
 
 const Services = () => {
   return (
@@ -51,127 +51,135 @@ const Services = () => {
         marginBottom={{ xs: 3 }}
       >
         <Grid item xs={12} sm={6} md={6} lg={3}>
-          <Card
-            sx={{
-              p: 3,
-              borderStyle: 'solid',
-              borderColor: '#FFC857',
-              borderWidth: '1px',
-              height: '100%',
-            }}
-          >
-            <Grid container direction={'column'} spacing={3}>
-              <Grid item>
-                <Typography
-                  fontWeight={700}
-                  fontSize={'1.25rem'}
-                  fontFamily={'Lato'}
-                >
-                  Idea
-                </Typography>
+          <CardActionArea href="/services#idea" sx={{ height: '100%' }}>
+            <Card
+              sx={{
+                p: 3,
+                borderStyle: 'solid',
+                borderColor: '#FFC857',
+                borderWidth: '1px',
+                height: '100%',
+              }}
+            >
+              <Grid container direction={'column'} spacing={3}>
+                <Grid item>
+                  <Typography
+                    fontWeight={700}
+                    fontSize={'1.25rem'}
+                    fontFamily={'Lato'}
+                  >
+                    Idea
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography fontFamily={'Lato'}>
+                    We meet with your team to know more about your idea, project
+                    and goal. After that, our team creates an action plan and
+                    proposal for your project.
+                  </Typography>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Typography fontFamily={'Lato'}>
-                  We meet with your team to know more about your idea, project
-                  and goal. After that, our team creates an action plan and
-                  proposal for your project.
-                </Typography>
-              </Grid>
-            </Grid>
-          </Card>
+            </Card>
+          </CardActionArea>
         </Grid>
 
         <Grid item xs={12} sm={6} md={6} lg={3}>
-          <Card
-            sx={{
-              p: 3,
-              borderStyle: 'solid',
-              borderColor: '#8D3B72',
-              borderWidth: '1px',
-              height: '100%',
-            }}
-          >
-            <Grid container direction={'column'} spacing={3}>
-              <Grid item>
-                <Typography
-                  fontWeight={700}
-                  fontSize={'1.25rem'}
-                  fontFamily={'Lato'}
-                >
-                  Design
-                </Typography>
+          <CardActionArea href="/services#design" sx={{ height: '100%' }}>
+            <Card
+              sx={{
+                p: 3,
+                borderStyle: 'solid',
+                borderColor: '#8D3B72',
+                borderWidth: '1px',
+                height: '100%',
+              }}
+            >
+              <Grid container direction={'column'} spacing={3}>
+                <Grid item>
+                  <Typography
+                    fontWeight={700}
+                    fontSize={'1.25rem'}
+                    fontFamily={'Lato'}
+                  >
+                    Design
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography fontFamily={'Lato'}>
+                    Your website's custom design will be tailored to your brand
+                    and target audience, ensuring that your online presence
+                    truly resonates with visitors.
+                  </Typography>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Typography fontFamily={'Lato'}>
-                  Your website's custom design will be tailored to your brand
-                  and target audience, ensuring that your online presence truly
-                  resonates with visitors.
-                </Typography>
-              </Grid>
-            </Grid>
-          </Card>
+            </Card>
+          </CardActionArea>
         </Grid>
 
         <Grid item xs={12} sm={6} md={6} lg={3}>
-          <Card
-            sx={{
-              p: 3,
-              borderStyle: 'solid',
-              borderColor: '#EC91D8',
-              borderWidth: '1px',
-              height: '100%',
-            }}
-          >
-            <Grid container direction={'column'} spacing={3}>
-              <Grid item>
-                <Typography
-                  fontWeight={700}
-                  fontSize={'1.25rem'}
-                  fontFamily={'Lato'}
-                >
-                  Development
-                </Typography>
+          <CardActionArea href="/services#development" sx={{ height: '100%' }}>
+            <Card
+              sx={{
+                p: 3,
+                borderStyle: 'solid',
+                borderColor: '#EC91D8',
+                borderWidth: '1px',
+                height: '100%',
+              }}
+            >
+              <Grid container direction={'column'} spacing={3}>
+                <Grid item>
+                  <Typography
+                    fontWeight={700}
+                    fontSize={'1.25rem'}
+                    fontFamily={'Lato'}
+                  >
+                    Development
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography fontFamily={'Lato'}>
+                    Translating your design into a responsive web presence, we
+                    ensure your websites is secure, performant, and optimized
+                    for search engines.
+                  </Typography>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Typography fontFamily={'Lato'}>
-                  Translating your design into a responsive web presence, we
-                  ensure your websites is secure, performant, and optimized for
-                  search engines.
-                </Typography>
-              </Grid>
-            </Grid>
-          </Card>
+            </Card>
+          </CardActionArea>
         </Grid>
 
         <Grid item xs={12} sm={6} md={6} lg={3}>
-          <Card
-            sx={{
-              p: 3,
-              borderStyle: 'solid',
-              borderColor: '#227C9D',
-              borderWidth: '1px',
-              height: '100%',
-            }}
-          >
-            <Grid container direction={'column'} spacing={3}>
-              <Grid item>
-                <Typography
-                  fontWeight={700}
-                  fontSize={'1.25rem'}
-                  fontFamily={'Lato'}
-                >
-                  Maintenance
-                </Typography>
+          <CardActionArea href="/services#maintenance" sx={{ height: '100%' }}>
+            <Card
+              sx={{
+                p: 3,
+                borderStyle: 'solid',
+                borderColor: '#227C9D',
+                borderWidth: '1px',
+                // height: '100%',
+              }}
+            >
+              <Grid container direction={'column'} spacing={3}>
+                <Grid item>
+                  <Typography
+                    fontWeight={700}
+                    fontSize={'1.25rem'}
+                    fontFamily={'Lato'}
+                  >
+                    Maintenance
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography fontFamily={'Lato'}>
+                    Our maintenance plans provide ongoing support to address
+                    technical concerns and content updates – giving your team
+                    the peace of mind to focus on more pressing matters.
+                  </Typography>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Typography fontFamily={'Lato'}>
-                  Our maintenance plans provide ongoing support to address
-                  technical concerns and content updates – giving your team the
-                  peace of mind to focus on more pressing matters.
-                </Typography>
-              </Grid>
-            </Grid>
-          </Card>
+            </Card>
+          </CardActionArea>
         </Grid>
       </Grid>
     </Grid>
