7b2da9a95fd6c870341dc7d9275f9225928beab2
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2023-11-21 10:12:54 -0500

    add seo keywords

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import DesignCompanyPage from './design-company';
 const IndexPage = () => {
   const seo = {
     title: 'blumi design | home',
-    description: 'website maintenance and repair for midsize companies.',
+    description: 'website design & repair for midsize companies.',
     keywords: [
       'web design in Midtown Toronto',
       'web design for small businesses',
